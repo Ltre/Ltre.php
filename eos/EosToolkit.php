a2e1774bcb20d59fbb1ac56e4a47991d8d835904
Ltre <ltrele@yeah.net>
2018-12-05 09:41:00 +0800

    1

--- eos/EosToolkit.php
+++ eos/EosToolkit.php
@@ -6,6 +6,11 @@ class EosToolkit {
     static function transferToken($from, $to, $amount, $memo = '', $contract = 'eosio.token'){
         $cmd = "cleos -u {$GLOBALS['eosUrl']} push action {$contract} transfer -j '[\"{$from}\" \"{$to}\" \"{$amount}\" \"{$memo}\"]' -p {$from}";
         $ret = Eos::execCmd($cmd);
+        $retData = json_decode($ret, 1) ?: [];
+        @$data = $retData['processed']['action_traces'][0]['act']['data'];
+        if (isset($retData['transaction_id']) && @$data['from'] == $from && @$data['to'] == $to) {
+
+        }
     }
 
 }
