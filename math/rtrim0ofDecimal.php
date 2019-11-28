e345167f2fac9f93dd83356c3af465d7bbb86eae
Ltre <ltrele@yeah.net>
2019-11-28 11:24:58 +0800

    rtrim0ofDecimal

--- math/rtrim0ofDecimal.php
+++ math/rtrim0ofDecimal.php
@@ -1,11 +1,11 @@
 <?php
 
 //去除小数后面多余的0
-function rtrim0ofDecimal($num){
+rtrim0ofDecimal($num){
     if (false !== strpos($num, '.')) {
         $num = rtrim(rtrim($num, '.0'), '0');
         return (float) $num;
     } else {
-        return (int) $num;
+        return intval(strval($num));//修正float转int精度偏差的问题
     }
 }
