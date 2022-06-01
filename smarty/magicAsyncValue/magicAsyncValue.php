e3df112ffb7b9c21e80ef93ea473cdcaaec34dd3
Ltre Oreki <ltrele@yeah.net>
2022-06-01 18:36:01 +0800

    Update magicAsyncValue.php

--- smarty/magicAsyncValue/magicAsyncValue.php
+++ smarty/magicAsyncValue/magicAsyncValue.php
@@ -3,6 +3,20 @@
 /**
  * 用于smarty输出异步数据的模板语法
  * 
+ * 逻辑概要：
+ *      1、为了减少在后端循环中计算额外数据而增加的浏览器等待时间，
+ *          特地将部分数据的获取过程转移到前端去做。
+ *      2、数据获取的过程，使用 [自定义PHP代码] 描述，并将此代码作为此方法的 $codeOrId 参数，
+ *          要求最后一句为return语句，且每句都要以分号结束，完全遵循PHP语法。
+ *      3、在smarty生成具体模板的同时，会将 [自定义PHP代码] 缓存，
+ *          并将缓存ID作为前端模板的<script>标签ID。
+ *      4、前端在获取标签ID对应的计算数据后，立即将自身<script>标签覆盖为数据。
+ * 
+ * 适用场景：
+ *      1、后台管理系统
+ *      2、开发懒人
+ *      3、非前台且用户量不大
+ * 
  * @param $codeOrId 
  *      mode采用set模式时，传自定义PHP代码；
  *      mode采用get模式时，传代码的缓存ID
@@ -24,13 +38,13 @@
  *          稍微复杂的例子：
  *              <{foreach $list as $v}>
  *                  <{* 这句可执行，但用到转义符比较繁琐 *}>
- *                  <{Utils::magicAsyncValue("return HardwareInfo::ver2batches(\"<{$v.hardware_ver}>\");") }>
+ *                  <{Utils::magicAsyncValue("return HardwareInfo::ver2batches(\"<{$v.hardware_ver}>\");") nofilter}>
  *                  <{* 这句可执行，推荐使用 *}>
- *                  <{Utils::magicAsyncValue("return HardwareInfo::ver2batches('<{$v.hardware_ver}>');") }>
+ *                  <{Utils::magicAsyncValue("return HardwareInfo::ver2batches('<{$v.hardware_ver}>');") nofilter}>
  *                  <{* 这句执行无效，不能提取v的属性 *}>
- *                  <{Utils::magicAsyncValue('return HardwareInfo::ver2batches("<{$v.hardware_ver}>")';) }>
+ *                  <{Utils::magicAsyncValue('return HardwareInfo::ver2batches("<{$v.hardware_ver}>")';) nofilter}>
  *                  <{* 这句执行无效，无法解析PHP数组元素的提取语法 *}>
- *                  <{Utils::magicAsyncValue("return HardwareInfo::ver2batches('{$v['hardware_ver']}')";) }>
+ *                  <{Utils::magicAsyncValue("return HardwareInfo::ver2batches('{$v['hardware_ver']}')";) nofilter}>
  *              <{/foreach}>
  *      准备一个HTTP(S)接口： /magic/async?id=xxx
  *          @exit(Utils::magicAsyncValue($_REQUEST['id'], 'get'));
@@ -40,10 +54,9 @@ function magicAsyncValue($codeOrId, $mode='set', $inf='/magic/async'){
     if ($mode == 'set') {
         $id = sha1(microtime(true).mt_rand());
         $hdl = __FUNCTION__.$id;
-        dwRedis::init('shoes')->setex($id, 60, $codeOrId);
+        $r->setex($id, 60, $codeOrId);
         return "<script id=\"{$id}\">$.get('{$inf}', {id:'{$id}'}, (j)=>{ $('#{$id}').replaceWith(j); });</script>";
     } else {
-        $r = dwRedis::init('shoes');
         if (! $codeOrId) return '';
         $code = $r->get($codeOrId);
         $r->del($codeOrId);
